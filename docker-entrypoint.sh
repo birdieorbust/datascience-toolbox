2ace89873b245f1f89faeba59b4e66408ccadf0a
Sam <sam.dc.hall@gmail.com>
2019-09-30 16:12:58 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-if [ $# -eq 0 ]
+if [ "$#" -eq 0 ]
   then
-    jupyter lab --ip=0.0.0.0 --NotebookApp.token='local-development' --allow-root --no-browser &> /dev/null &
+    jupyter lab --ip=0.0.0.0 --allow-root --no-browser &> /dev/null &
+    code-server1.1156-vsc1.33.1-linux-x64/code-server --allow-http --no-auth --data-dir /data /code
   else
     exec "$@"
 fi
